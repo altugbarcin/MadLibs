599a401e27fc4d0bd5d160b8b87b1629f494d69f
altugbarcin <altugbrcn@gmail.com>
2023-08-25 15:54:08 +0300

    datainputmadlibs
    MadLibs program..

---
+++ madlibs.py
@@ -0,0 +1,25 @@
+"""
+adj = input("Adjective: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+famous_person = input("Famous Person: ")
+
+madlib = f"Computer programming is so {adj}! It makes me so excited all the time because \
+I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}!"
+
+print(madlib)
+"""
+
+
+name = input("Name: ")
+surname = input("Surname: ")
+
+acceptedmessage = f"Hello dear {name} {surname}. The City University accepted you. You need to complete your registration as soon as possible."
+
+print(acceptedmessage)
+                    
+                    
+
+                    
+
+
